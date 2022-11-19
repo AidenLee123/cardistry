5570cc894263ecf571fe1ed4db9739b54b041224
ellie <ellie@kmong.com>
2022-11-19 10:16:30 +0900

    edit loginpage

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,24 +1,28 @@
-import styled from "styled-components"
-import React, {useState} from "react"
-import Ribbonspread from "./ribbon.png"
+import styled from "styled-components";
+import React, { useState } from "react";
+import Ribbonspread from "./ribbon.png";
 
-const Wrapper =  styled.div`
-width: 100vw;
-height: 100vh;
-background-image:url(${Ribbonspread});
-background-size: cover;
-display: flex;
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${Ribbonspread});
+  background-size: cover;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 const Title = styled.h1`
-font-size:50px;
-color: white;
-`
+  font-size: 50px;
+  color: white;
+`;
 
-      
 function LoginPage() {
-    return (<Wrapper><Title>Expert at card table</Title>
-    </Wrapper>)
+  return (
+    <Wrapper>
+      <Title>Expert at card table</Title>
+    </Wrapper>
+  );
 }
 
 export default LoginPage;
