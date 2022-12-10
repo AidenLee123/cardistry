ea3c9b327bc11d522287c05d86a52d2ffe5ade28
이승준 <AidenLee@iseung-euns-MacBook-Air.local>
2022-12-10 10:56:25 +0900

    code

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -4,7 +4,7 @@ import "./index.scss";
 import Card from "./card.png";
 
 const Wrapper = styled.div`
-  background-color: #ebd681;
+  background-color: #A9A9A9; //todo
   height: 100vh;
   display: flex;
   flex-direction: column;
@@ -43,13 +43,35 @@ function MainPage() {
       <div class="threeholder">
         <div class="threeone">
           <MagicTitle className="title">Magic</MagicTitle>
-          <ListText>1. AAAA</ListText>
-          <ListText>1. AAAA</ListText>
-          <ListText>1. AAAA</ListText>
-          <ListText>1. AAAA</ListText>
+          <ListText>1. ACAN</ListText>
+          <ListText>2. lie detector</ListText>
+          <ListText>3. Red Hot Mama</ListText>
+          <ListText>4. Drunk Magician</ListText>
+          <ListText>5. Ace on top</ListText>
+          <ListText>6. Ambitious Card Magic</ListText>
+          <ListText>7. Out of this World</ListText>
+          <ListText>8. Arobatic Ace</ListText>
+          <ListText>9. Mental magic</ListText>
+          <ListText>10. Smart Phone magic</ListText>
+
+
         </div>
         <div class="threeone">
-          <MagicTitle className="title">TITLE 1</MagicTitle>
+          <MagicTitle className="title">Gambling Techniques</MagicTitle>
+          <ListText>1. Casino Shuffle Sequence</ListText>  
+          <ListText>2. Different grips by country</ListText>  
+          <ListText>3. Ribbon spread</ListText>  
+          <ListText>4. Dealing at Texas Hold'em</ListText>  
+          <ListText>5. Fasle Shuffle</ListText> 
+          <ListText>6. Nullifying the cut</ListText>  
+          <ListText>7. Bottom Deal</ListText>  
+          <ListText>8. How to gather the high cards from the shuffled cards</ListText>
+          <ListText>9. Stacking the Deck</ListText>
+          <ListText>10.Mucking cards</ListText>
+          <ListText>11.How to manipulate the opponent's shuffled cards</ListText>
+          <ListText>12.Three Card Monte</ListText>
+          <ListText>13.Card Sping</ListText>
+          <ListText>14.One hand Shuffle and Cut</ListText>
         </div>
       </div>
     </Wrapper>
