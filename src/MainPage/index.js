128f0a40e12c837260cf8f10ead532afb5a8f97a
ellie <ellie@kmong.com>
2022-12-10 10:15:37 +0900

    [Backend] Release 1.6.4

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,67 +1,59 @@
 import styled from "styled-components";
 import React, { useState } from "react";
+import "./index.scss";
+import Card from "./card.png";
 
 const Wrapper = styled.div`
-  background-color: white;
+  background-color: #ebd681;
   height: 100vh;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  `
-  const Title = styled.h1`
-font size: 100px;
-color:Black;
-margin-bottom:100px;
-`
-
-const MiddleWrapper = styled.div`
-width: 100%;
-display:flex;
-flex-direction: row;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+  color: Black;
+  margin-bottom: 100px;
+  border-bottom: 4px solid black;
 `;
 
-const LeftWrapper = styled.div`
-width: 50%;
-text-align:center;
+const MagicTitle = styled.div`
+  width: 200px;
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 45px;
+  margin-left: 80px;
+  /* text-align: center; */
+  color: white;
 `;
-const MagicTitle=styled.div`
-font-size: 35px;
+
+const ListText = styled.p`
+  width: 200px;
+  text-align: center;
+  margin-top: 20px;
+  font-size: 22px;
+  color: #8a8a8a;
+  letter-spacing: 1px;
 `;
-const Text = styled.div`
-font-size: 18px;
-`;
-const RightWrapper = styled.div`
-width: 50%;
-text-align:center;
-`;
-const GamblingtechniquesTitle = styled.div`
-font-size:35px;
-`;
+
 function MainPage() {
-  return <Wrapper>
-          <Title>Expert at card table</Title>
-          <MiddleWrapper>
-          <LeftWrapper>
-            <MagicTitle>Magic</MagicTitle>
-            
-            <Text>1.Ace on top</Text>
-            <Text>2. How to change 666666 to 999999!</Text>
-            <Text>3. How to make make my phone number by other people's personal information</Text>
-            <Text>4. Acaan</Text>
-            <Text>5. How to match the card by reading other people's mind</Text>
-            <Text>6.Ambitious</Text>
-            <Text>7.Counthing the Aces</Text>
-            <Text>8.King assembly</Text>
-
-            </LeftWrapper>
-
-          <RightWrapper>
-          <GamblingtechniquesTitle>Gambling Techniques</GamblingtechniquesTitle>
-          </RightWrapper>
-
-          </MiddleWrapper>
-
-          </Wrapper>;
+  return (
+    <Wrapper>
+      <Title>Expert at card</Title>
+      <div class="threeholder">
+        <div class="threeone">
+          <MagicTitle className="title">Magic</MagicTitle>
+          <ListText>1. AAAA</ListText>
+          <ListText>1. AAAA</ListText>
+          <ListText>1. AAAA</ListText>
+          <ListText>1. AAAA</ListText>
+        </div>
+        <div class="threeone">
+          <MagicTitle className="title">TITLE 1</MagicTitle>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default MainPage;
