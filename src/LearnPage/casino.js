24efc508bb0a333182b2838173decdfe58e63eb7
ellie <ellie@kmong.com>
2023-01-07 10:46:30 +0900

    [Backend] Release 1.9.4

--- src/LearnPage/casino.js
+++ src/LearnPage/casino.js
@@ -5,6 +5,7 @@ import Magic from "../Images/magic.jpeg";
 
 import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
+import "./index.css";
 
 const Wrapper = styled.div`
   background-color: white;
@@ -23,13 +24,13 @@ const Title = styled.div`
 `;
 
 const ImageWrapper = styled.img`
-  width: 100%;
+  width: 800px;
   height: 300px;
 `;
 function CasinoPage() {
   const images = [
     "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "../Images/magic.jpeg",
+    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
   ];
 
   return (
